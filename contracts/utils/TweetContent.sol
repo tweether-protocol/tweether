8efdf2d343e3620ed5cdf0bab9eac9f0fbbbe43e
Alex Roan <alex.roan@hotmail.com>
2020-10-07 14:51:16 +0100

    Added TweetContent library to measure length of tweet

---
+++ contracts/utils/TweetContent.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.10;
+
+library TweetContent {
+    function length(string memory s) internal pure returns (uint)
+    {
+        uint len = 0;
+        uint i = 0;
+        bytes memory strBytes = bytes(s);
+
+        while (i < strBytes.length) {
+            if (strBytes[i]>>7 == 0) {
+                i+=1;
+            } else if (strBytes[i]>>5 == 0x06) {
+                i+=2;
+            } else if (strBytes[i]>>4 == 0x0E) {
+                i+=3;
+            } else if (strBytes[i]>>3 == 0x1E) {
+                i+=4;
+            } else {
+                i+=1;
+            }
+            len++;
+        }
+        return len;
+    }
+}
