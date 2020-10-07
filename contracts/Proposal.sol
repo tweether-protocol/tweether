f5c395e86a8636eb70674eeb52dc69c062f2d652
Alex Roan <alex.roan@hotmail.com>
2020-10-07 16:15:52 +0100

    Implemented newTweet

--- contracts/Proposal.sol
+++ contracts/Proposal.sol
@@ -13,20 +13,37 @@ import "./utils/TweetContent.sol";
 contract Proposal is ERC721, Ownable{
     using TweetContent for string;
     using Counters for Counters.Counter;
+
     Counters.Counter private _tokenIds;
+
+    Tweet[] public tweets;
+
+    struct Tweet {
+        address proposer;
+        uint expiry;
+        string content;
+        bool accepted;
+    }
 
     constructor() public ERC721("Tweether Tweet", "NFTWE") {
     }
 
     function newTweet(address proposer, uint expiry, string memory content) external onlyOwner returns (uint) {
         require(content.fitsInTweet(), "Invalid tweet size");
+        _tokenIds.increment();
+        uint256 newId = _tokenIds.current();
+        tweets.push(
+            Tweet(proposer, expiry, content, false)
+        );
+        _safeMint(owner(), newId);
+        return newId;
     }
 
     function resetExpiry(uint tokenId, uint expiry) external onlyOwner {
 
     }
 
-    function tweetAccepted(uint tokenId) external onlyOwner {
+    function acceptTweet(uint tokenId) external onlyOwner {
 
     }
 
